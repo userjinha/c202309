df54f9e08dbcc62bc2285b99a122ac21457fbfe9
userjinha <143943345+userjinha@users.noreply.github.com>
2023-09-15 11:50:40 +0900

    operation.c

--- practice/week2/operation.c
+++ practice/week2/operation.c
@@ -4,10 +4,13 @@ int main()
 {
 	int x = 4;
 	int y = 2;
+	// 정수 변수 x와 y를 선언하고 초기값을 설정
 	int z;
+	// 정수 변수 z를 선언
 
 	z = x + y;
 	printf("z = x + y = %d\n", z);
+	// x와 y의 합을 계산하여 z에 저장하고, z값 출력
 	z = x - y;
 	printf("z = x - y = %d\n", z);
 	z = x * y;
