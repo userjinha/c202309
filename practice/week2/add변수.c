fdcfc6b845a56da93f4ab51f0ee8efe1c55ef623
userjinha <143943345+userjinha@users.noreply.github.com>
2023-09-15 09:40:34 +0900

    add변수.c

--- practice/week2/add변수.c
+++ practice/week2/add변수.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+// 전처리기 지시문 : 컴파일 전 수행작업, #로 시작하고 <>로 관련 파일을 추가
+// stdio.h C표준 입출력 함수를 포함하는 헤더파일
+int main(void)
+// main이라는 이름의 함수 정의, int : main()함수가 반환하는 값의 자료형 
+// void : 매개변수가 없음을 명시하는 키워드 (매개변수 : 함수에 입력되는 변수)
 
-int main(void) {
+{
 		int add = 3 + 7;
 	return 0;
 		}
