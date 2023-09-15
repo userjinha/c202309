cf1db119ab3ec72cb78fc42e7b8edfb63d162587
userjinha <143943345+userjinha@users.noreply.github.com>
2023-09-15 09:53:38 +0900

    문자형변수.c

--- practice/week2/문자형변수.c
+++ practice/week2/문자형변수.c
@@ -2,6 +2,9 @@
 
 int main(void) {
 	char c = 'A';
-	printf("%c��n", c);
+	// 문자형 변수 출력, 문자형 변수 유형인 char로 선언된 c 변수를 초기화
+	printf("%c\n", c);
+	// printf 함수를 사용하여 문자형 출력, %c는 문자 값을 출력하기 위한 서식 지정자
+	// , 다음에 'c'변수를 전달하여 c변수의 값을 출력
 	return 0;
 }
