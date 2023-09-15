10542012671343f9359de7d8a1daab79beabe875
userjinha <143943345+userjinha@users.noreply.github.com>
2023-09-15 12:44:58 +0900

    variable.c

--- practice/week2/variable.c
+++ practice/week2/variable.c
@@ -1,31 +1,20 @@
-﻿
-Microsoft Visual Studio Solution File, Format Version 12.00
-# Visual Studio Version 17
-VisualStudioVersion = 17.7.34024.191
-MinimumVisualStudioVersion = 10.0.40219.1
-Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "프랙", "프랙\프랙.vcxproj", "{CD5E1BFD-B1A9-4C98-BA1C-26B402469CFB}"
-EndProject
-Global
-	GlobalSection(SolutionConfigurationPlatforms) = preSolution
-		Debug|x64 = Debug|x64
-		Debug|x86 = Debug|x86
-		Release|x64 = Release|x64
-		Release|x86 = Release|x86
-	EndGlobalSection
-	GlobalSection(ProjectConfigurationPlatforms) = postSolution
-		{CD5E1BFD-B1A9-4C98-BA1C-26B402469CFB}.Debug|x64.ActiveCfg = Debug|x64
-		{CD5E1BFD-B1A9-4C98-BA1C-26B402469CFB}.Debug|x64.Build.0 = Debug|x64
-		{CD5E1BFD-B1A9-4C98-BA1C-26B402469CFB}.Debug|x86.ActiveCfg = Debug|Win32
-		{CD5E1BFD-B1A9-4C98-BA1C-26B402469CFB}.Debug|x86.Build.0 = Debug|Win32
-		{CD5E1BFD-B1A9-4C98-BA1C-26B402469CFB}.Release|x64.ActiveCfg = Release|x64
-		{CD5E1BFD-B1A9-4C98-BA1C-26B402469CFB}.Release|x64.Build.0 = Release|x64
-		{CD5E1BFD-B1A9-4C98-BA1C-26B402469CFB}.Release|x86.ActiveCfg = Release|Win32
-		{CD5E1BFD-B1A9-4C98-BA1C-26B402469CFB}.Release|x86.Build.0 = Release|Win32
-	EndGlobalSection
-	GlobalSection(SolutionProperties) = preSolution
-		HideSolutionNode = FALSE
-	EndGlobalSection
-	GlobalSection(ExtensibilityGlobals) = postSolution
-		SolutionGuid = {D74BF839-45BE-4248-8E73-703CC547F62A}
-	EndGlobalSection
-EndGlobal
+#include <stdio.h>
+
+int main()
+{
+	int x;
+        int y;
+        int z;
+
+         x=1;
+         y=2;
+         // 변수에 값을 대입 
+	z = x + y;
+	// 변수 값을  더한 결과를 변수에 저장
+
+         printf("%d", z);
+	// 정수 z값을 출력
+
+         return 0;
+}
+
