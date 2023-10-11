328d412d550d94c5b414da66fa92fd4f228055d2
userjinha <143943345+userjinha@users.noreply.github.com>
2023-10-11 23:59:51 +0900

    Update studentGade.c

--- challenge/week6/studentGade.c
+++ challenge/week6/studentGade.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #define STUDENTS 5
 
-// 학생성적을 분류하고 그에 따른 점수를 출력함
+// 학생 성적을 분류하고 그에 따른 학점을 출력하는 함수
 void classifyStudents(int scores[], char targetGrade) {
 	printf("학생 성적 분류:\n");
 	char grade = ' ';
@@ -21,76 +21,72 @@ void classifyStudents(int scores[], char targetGrade) {
 		else {
 			grade = 'F';
 		}
-		if (targetGrade == grade) {
-			printf("%d 학생은 %c점수를 받았습니다.", i + 1, targetGrade);
-		}
+		
+		if (targetGrade == grade)
+		{
+			printf("%d 학생은 %c 학점을 받았습니다.\n", i + 1, targetGrade);
+		} // 만약 입력받은 값과 grade가 같다면 이 문장이 출력된다.
 	}
 }
 
-// 입력받은 값을 계산한다음 sum 값을 메인함수에 반환
+// 학생 성적의 총합을 계산한 뒤 반환하는 함수
 int sumScores(int scores[]) {
-
 	int sum = 0;
 	for (int i = 0; i < STUDENTS; i++) {
 		sum += scores[i];
-		
-
 	}
 	return sum;
 }
 
-// 입력받은 값을 계산한다음 average 값을 메인함수에 반환
+// 학생 성적의 평균을 계산한 뒤 반환하는 함수
 double averageScores(int scores[]) {
 	int sum = 0;
 	double average;
-	for(int i =0; i<STUDENTS; i++){
+	for (int i = 0; i < STUDENTS; i++) {
 		sum += scores[i];
 	}
 	average = (double)sum / STUDENTS;
 	return average;
 }
 
-// 입력받은 값에 순위를 매긴다음 문장 출력
+// 학생 성적에 대한 순위를 계산하고 출력하는 함수
 void printRanks(int scores[]) {
 	int ranks[STUDENTS];
 	for (int i = 0; i < STUDENTS; i++) {
-		ranks[i] = 1;
+		ranks[i] = 1; // 순위를 매기기 위해 모두 일단 1로 초기화
 	}
 	for (int i = 0; i < STUDENTS; i++) {
-		for (int j = 0; i < STUDENTS; i++) {
+		for (int j = 0; j < STUDENTS; j++) {
 			if (scores[i] < scores[j]) {
-				ranks[i]++;
+				ranks[i]++; // 만약 비교하여 더 크다면 순위 증감
 			}
 		}
-		printf("%d 학생의 순위는 %d 입니다.", i + 1, ranks[i]);
+		printf("%d 학생의 순위는 %d 입니다.\n", i + 1, ranks[i]); // 문장 출력
 	}
-	
-
-
 }
 
 int main() {
-	int scores[STUDENTS]; //학생 점수의 배열을 초기화
+	int scores[STUDENTS]; // 학생 점수를 저장하는 배열
 
 	for (int i = 0; i < STUDENTS; i++) {
-		printf("학생 %d의 성적을 입력하세요: ", i + 1); // 문장 출력
-		scanf_s("%d", &scores[i]); //입력받은 값을 배열에 저장
+		printf("학생 %d의 성적을 입력하세요: ", i + 1);
+		scanf_s("%d", &scores[i]);
 	}
 
-	char ch = getchar(); //버퍼 임시 저장 변수. 엔터 지우기 위해
+	char ch;
+	while ((ch = getchar()) != '\n' && ch != EOF); // 버퍼 비우기
 
-	char target;// 특정 점수의 값 저장을 위한 변수 선언
-	printf("특정 점수(A, B, C, D, F)를 입력하시오:");
-	scanf_S("%c", &target, 1);
+	char target;
+	printf("특정 학점(A, B, C, D, F)을 입력하시오: "); 
+	scanf_s(" %c", &target); // 입력받은 값을 특정학점 변수에 저장
 
-	classifystudents(scores, target); // 값을 함수에 반환
-	
+	classifyStudents(scores, target); // 이 함수의 변수에 값을 반환
 
-	int sum = sumScores(scores); // 반환 받은 값을 저장
-	double average = averageScores(scores); // 반환 받을 값을 저장
-	pirntf("학생들 점수의 총 합은 %d 이고, 평균 값은 %lf입니다.\n", sum, average); // 문장 출력
+	int sum = sumScores(scores); //값을 받고 저장
+	double average = averageScores(scores); // average 값을 받고 저장
+	printf("학생들 점수의 총 합은 %d이고, 평균 값은 %.2lf입니다.\n", sum, average);
 
-	printfRanks(scores); // 값을 함수에 반환
+	printRanks(scores); // 값을 반환
 
 	return 0;
 }
