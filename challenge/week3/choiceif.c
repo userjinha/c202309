55e8ab8124b62ba85e146e42040ec655ae48d180
userjinha <143943345+userjinha@users.noreply.github.com>
2023-11-05 21:24:55 +0900

    Update choiceif.c

--- challenge/week3/choiceif.c
+++ challenge/week3/choiceif.c
@@ -1,41 +1,42 @@
 #include <stdio.h>
 
 int main() {
-	int choice;
-	//choice 라는 변수 선언
+	int choice;//choice 라는 변수 선언
 
-	printf("1. 파일 저장\n ");
-	// 1. 파일저장이 출력
+
+	printf("1. 파일 저장\n ");// 1. 파일저장이 출력
+
+
+	printf("2. 저장 없이 닫기\n");// 2. 저장 없이 닫기가 출력
 	
-	printf(" 2. 저장 없이 닫기\n");
-	// 2. 저장 없이 닫기가 출력
-	printf("3. 종료\n");
-	// 3. 종료가 출력됨
-	scanf_s("%d", &choice);
-	// 입력된 값이 값이 choice에 저장됨
+	printf("3. 종료\n");// 3. 종료가 출력됨
 
-	if (choice = 1) {
-		printf("파일을 저장합니다.");
-		// choice가 1이면 파일을 저장합니다가 띈다.
-	}
-	else if (choice =2) {
-		printf(" 저장 없이 닫습니다.");
-		// choice가 2이면 저장 없이 닫습니다가 띈다.
-	}
-	else if (choice =3) {
-		printf(" 종료합니다.");
-	}
-		 // 3이면 종료합니다가 띈다.
-	else {
-		printf("잘못 입력하셨습니다.");
-		}
-	// 다른거면 잘못 입력하셨습니다가 띈다.
-		return 0;
+	scanf_s("%d", &choice);// 입력된 값이 값이 choice에 저장됨
+	
 
+	if (choice == 1) {
+		printf("파일을 저장합니다."); // choice가 1이면 파일을 저장합니다가 띈다.
 		
-	
+	}
+	else if (choice == 2) {
+		printf(" 저장 없이 닫습니다.");// choice가 2이면 저장 없이 닫습니다가 띈다.
+
+	}
+	else if (choice == 3) {
+		printf(" 종료합니다.");	// 3이면 종료합니다가 띈다.
+	}
+
+	else {
+		printf("잘못 입력하셨습니다.");// 다른거면 잘못 입력하셨습니다가 띈다.
+	}
+
+	return 0;
+
+
+
 
 }
+
 		
 	
 
